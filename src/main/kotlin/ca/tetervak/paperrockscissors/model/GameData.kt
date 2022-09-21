ba94be080c2565f89278c7f8da54b245f6297a50
Alexander Tetervak <tetervak@sheridancollege.ca>
2022-09-21 08:51:46 -0400

    rewrote GameData constructors

--- src/main/kotlin/ca/tetervak/paperrockscissors/model/GameData.kt
+++ src/main/kotlin/ca/tetervak/paperrockscissors/model/GameData.kt
@@ -1,29 +1,38 @@
 package ca.tetervak.paperrockscissors.model
 
-data class GameData(val playerOneChoice: Choice, val playerTwoChoice: Choice){
+data class GameData(
+    val playerOneChoice: Choice,
+    val playerTwoChoice: Choice,
+    val result: Result
+) {
 
-    val result: Result = getGameResult()
+    //val result: Result = getGameResult()
+    constructor(playerOneChoice: Choice, playerTwoChoice: Choice) : this(
+        playerOneChoice, playerTwoChoice, getGameResult(playerOneChoice, playerTwoChoice)
+    )
 
-    private fun getGameResult(): Result {
-        return when (playerOneChoice){
-            Choice.PAPER -> when(playerTwoChoice){
-                Choice.PAPER -> Result.REPLAY
-                Choice.ROCK -> Result.PLAYER_ONE_WINS
-                Choice.SCISSORS -> Result.PLAYER_TWO_WINS
-            }
-            Choice.ROCK -> when(playerTwoChoice){
-                Choice.PAPER -> Result.PLAYER_TWO_WINS
-                Choice.ROCK -> Result.REPLAY
-                Choice.SCISSORS -> Result.PLAYER_ONE_WINS
-            }
-            Choice.SCISSORS -> when(playerTwoChoice){
-                Choice.PAPER -> Result.PLAYER_ONE_WINS
-                Choice.ROCK -> Result.PLAYER_TWO_WINS
-                Choice.SCISSORS -> Result.REPLAY
+    companion object {
+        private fun getGameResult(playerOneChoice: Choice, playerTwoChoice: Choice): Result {
+            return when (playerOneChoice) {
+                Choice.PAPER -> when (playerTwoChoice) {
+                    Choice.PAPER -> Result.REPLAY
+                    Choice.ROCK -> Result.PLAYER_ONE_WINS
+                    Choice.SCISSORS -> Result.PLAYER_TWO_WINS
+                }
+
+                Choice.ROCK -> when (playerTwoChoice) {
+                    Choice.PAPER -> Result.PLAYER_TWO_WINS
+                    Choice.ROCK -> Result.REPLAY
+                    Choice.SCISSORS -> Result.PLAYER_ONE_WINS
+                }
+
+                Choice.SCISSORS -> when (playerTwoChoice) {
+                    Choice.PAPER -> Result.PLAYER_ONE_WINS
+                    Choice.ROCK -> Result.PLAYER_TWO_WINS
+                    Choice.SCISSORS -> Result.REPLAY
+                }
             }
         }
     }
 
-    override fun toString() =
-        "GameData(playerOneChoice=$playerOneChoice, playerTwoChoice=$playerTwoChoice, result=$result)"
 }
